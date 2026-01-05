f0eb760bbb18ae95da669d9838b8c1692ec693f9
hminh <hminh250104@gmail.com>
2026-01-06 02:20:51 +0700

    feat: Add initial UI for weather and settings screens
    Adds the basic layouts and fragments for the main weather display and a settings screen.
    
    - Creates `MainActivity` and its corresponding layout.
    - Adds `WeatherFragment` and `SettingFragment` with their initial layout files.
    - Defines a navigation graph (`app_nav_graph.xml`) to handle navigation between the weather and settings fragments.
    - Populates the layouts with placeholder UI components for displaying weather information and settings options.

---
+++ app/src/main/java/hminq/dev/weatherapp/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package hminq.dev.weatherapp.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import hminq.dev.weatherapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
