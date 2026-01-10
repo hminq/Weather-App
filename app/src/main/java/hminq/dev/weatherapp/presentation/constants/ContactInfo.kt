fc944c0d32b57e1c51720b713f84fe22407d6493
hminh <hminh250104@gmail.com>
2026-01-10 21:52:13 +0700

    feat: Add contact information constants

---
+++ app/src/main/java/hminq/dev/weatherapp/presentation/constants/ContactInfo.kt
@@ -0,0 +1,11 @@
+package hminq.dev.weatherapp.presentation.constants
+
+object ContactInfo {
+    const val EMAIL_ADDRESS = "hungminh.204.dev@gmail.com"
+    const val GITHUB_URL = "https://github.com/hminq"
+    const val GITHUB_PACKAGE_NAME = "com.github.android"
+    const val INSTAGRAM_URL = "https://www.instagram.com/toilam0nesy/"
+    const val INSTAGRAM_PACKAGE_NAME = "com.instagram.android"
+    const val TWITTER_URL = "https://x.com/uistonmi"
+    const val TWITTER_PACKAGE_NAME = "com.twitter.android"
+}
