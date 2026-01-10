9a987538e89ab9135e289788a3fa01477ecba912
hminh <hminh250104@gmail.com>
2026-01-10 21:58:26 +0700

    feat: Enable Hilt for MainActivity

--- app/src/main/java/hminq/dev/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/hminq/dev/weatherapp/presentation/MainActivity.kt
@@ -2,8 +2,10 @@ package hminq.dev.weatherapp.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 import hminq.dev.weatherapp.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
