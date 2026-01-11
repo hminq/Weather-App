df0f768e63fcae6cd2d3b9dbd68810a143e64b25
hminh <hminh250104@gmail.com>
2026-01-12 01:01:31 +0700

    refactor: Remove Framework stuffs from Domain layer

--- app/src/main/java/hminq/dev/weatherapp/domain/exception/DomainException.kt
+++ app/src/main/java/hminq/dev/weatherapp/domain/exception/DomainException.kt
@@ -1,10 +1,6 @@
 package hminq.dev.weatherapp.domain.exception
 
-import androidx.annotation.StringRes
-import hminq.dev.weatherapp.R
-
 sealed class DomainException(
     message: String? = null,
-    cause: Throwable? = null,
-    @StringRes val messageResId: Int? = R.string.general_error
+    cause: Throwable? = null
 ) : Exception(message, cause)
