e30803b3d04c74c7dffc332e88757b5fd0e15cc7
hminh <hminh250104@gmail.com>
2026-01-15 03:22:50 +0700

    refactor: small changes

--- app/src/main/java/hminq/dev/weatherapp/di/RepositoryModule.kt
+++ app/src/main/java/hminq/dev/weatherapp/di/RepositoryModule.kt
@@ -4,8 +4,12 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import hminq.dev.weatherapp.data.repository.LocationManager
 import hminq.dev.weatherapp.data.repository.UserSettingRepositoryImpl
+import hminq.dev.weatherapp.data.repository.WeatherRepositoryImpl
+import hminq.dev.weatherapp.domain.repository.LocationRepository
 import hminq.dev.weatherapp.domain.repository.UserSettingRepository
+import hminq.dev.weatherapp.domain.repository.WeatherRepository
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -14,4 +18,14 @@ abstract class RepositoryModule {
     abstract fun bindUserSettingRepository(
         impl: UserSettingRepositoryImpl
     ): UserSettingRepository
+
+    @Binds
+    abstract fun bindWeatherRepository(
+        impl: WeatherRepositoryImpl
+    ): WeatherRepository
+
+    @Binds
+    abstract fun bindLocationRepository(
+        impl: LocationManager
+    ): LocationRepository
 }
